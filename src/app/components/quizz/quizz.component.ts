fc4212fe71c17ca392edcd1f1e2c7995e616a070
bertolaaline <bertola.aline@gmail.com>
2024-01-09 17:23:15 -0300

    Primeiro commit

---
+++ src/app/components/quizz/quizz.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-quizz',
+  standalone: true,
+  imports: [],
+  templateUrl: './quizz.component.html',
+  styleUrl: './quizz.component.css'
+})
+export class QuizzComponent {
+  title:string = ""
+
+  questions: any
+  questionSelected: any 
+}
