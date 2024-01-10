7c9c7e501dee9ae91f00e4e2cda9746038cce60e
bertolaaline <bertola.aline@gmail.com>
2024-01-10 10:35:01 -0300

    Cria regras do jogo

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import quizz_questions from './../../../assets/data/quizz_questions.json'
 
 @Component({
   selector: 'app-quizz',
@@ -8,8 +9,39 @@ import { Component } from '@angular/core';
   styleUrl: './quizz.component.css'
 })
 export class QuizzComponent {
-  title:string = ""
+  title:string
+  questionIndex:number
+  questionMaxIndex:number
 
   questions: any
-  questionSelected: any
+  questionSelected: any 
+
+  answers:string[] = []
+  answerSelected:string= ""
+  
+  finished:boolean
+
+  constructor () {
+    this.title = quizz_questions.title
+    this.questions = quizz_questions.questions
+    this.questionIndex = 0
+    this.questionSelected = this.questions[this.questionIndex]
+    this.questionMaxIndex = this.questions.length -1
+    this.finished = false
+
+  }
+
+  public buttonPress(alias:string) {
+    this.answers.push(alias)
+  } 
+
+  async nextStep() {
+    this.questionIndex+=1
+
+    if(this.questionMaxIndex > this.questionIndex) {
+        this.questionSelected = this.questions[this.questionMaxIndex]
+    }else {
+      this.finished = true
+    }
+  }
 }
